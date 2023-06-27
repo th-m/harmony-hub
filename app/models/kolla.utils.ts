3409b599bd08b3aa24c48bcb921ffa09655792ac
th-m <thomvaladez@gmail.com>
2023-06-27 13:52:52 -0600

    readme and rescuetime

--- app/models/kolla.utils.ts
+++ app/models/kolla.utils.ts
@@ -3,12 +3,14 @@ export const fitbitConnectorId = "fitbit-99562";
 export const githubConnectorID = "github-88436";
 export const linearConnectorID = "linear-783";
 export const openaiConnectorID = "openai-apikey-27922";
+export const rescueTimeID = "rescuetime-55002";
 export const connectorIDs = [
   wakatimeConnectorId,
   fitbitConnectorId,
   githubConnectorID,
   linearConnectorID,
   openaiConnectorID,
+  rescueTimeID,
 ] as const;
 
 export type ConnectorID = typeof connectorIDs[number]
